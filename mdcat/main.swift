e04ffecb3d4af4e03d91ea406b06f3b6c6924b90
David Rauch <mail@davidrauch.at>
2016-10-02 15:34:20 +0200

    Initial Commit

---
+++ mdcat/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mdcat
+//
+//  Created by David Rauch on 02/10/2016.
+//  Copyright © 2016 David Rauch. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
